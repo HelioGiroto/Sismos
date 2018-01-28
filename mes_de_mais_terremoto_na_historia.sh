7aa74bded276f11977e49097ff047b305ff7a6f6
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-01-28 02:19:24 -0200

    Add files via upload

---
+++ mes_de_mais_terremoto_na_historia.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo > sismos_meses.dat
+
+for ano in {1950..2018}
+do
+	echo "-$ano:" >> sismos_meses.dat
+	for mes in {01..12}
+	do
+
+		grep -Ec "$ano-$mes-.." sismos5+ >> sismos_meses.dat
+
+	done
+	grep -Ec "$ano-....." sismos5+ >> sismos_meses.dat
+#	echo >> sismos_meses.dat
+done
+
+cat sismos_meses.dat | tr '\n' ' ' | tr '-' '\n' > tot-sismos.txt
+sed -i 1d tot-sismos.txt
+
+
+
+
+# PROGRAMA # 2:
+
+for m in {2..14}
+do
+	awk -v m=$m '{tot+=$m} END {print tot}' tot-sismos.txt >> tot-meses.dat
+done
+
+echo >> tot-sismos.txt
+echo "=====================================================" >> tot-sismos.txt
+echo "TOTAL: " $(cat tot-meses.dat | xargs) >> tot-sismos.txt
+
+rm *.dat
+
