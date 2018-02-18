667b0528980bed14df03d6ee2cf9399883a2f6a3
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-02-18 19:24:20 -0300

    Update alerta_sismico.sh

--- alerta_sismico.sh
+++ alerta_sismico.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ESSE ESCRIPT RODA SOMENTE EM MAC...
+# ESSE ESCRIPT RODA EM MAC...
 
 # Avisará todos os terremotos acima de magnitude 2. 
 # Caso quiera alterar a magnitude, modificar linhas 13 e 15:
@@ -15,6 +15,15 @@ do
 	local=$(curl -sX GET 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&minmagnitude=2&starttime=yesterday' | awk -F'|' '{print $2 "|" $11 "|" $9 "|" $13}' | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f4)
 		
 	[[ $hora == $hora_anterior ]] || { afplay beep.mp3; hora_anterior=$hora; open -a Safari https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
+
+	# No Raspberry Pi:
+	# [[ $hora == $hora_anterior ]] || { omxplayer beep.mp3; hora_anterior=$hora; open -a Safari https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
+	
+	# No Linux:
+	# [[ $hora == $hora_anterior ]] || { cvlc --play-and-exit beep.mp3 ; hora_anterior=$hora; open -a Safari https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
+
+
+
 
 	sleep 10
 
