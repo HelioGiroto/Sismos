6c1517d170c1f978d9bb6c6fa821117fa599673f
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-02-18 21:45:14 -0300

    Update alerta_sismico.sh

--- alerta_sismico.sh
+++ alerta_sismico.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# ESSE ESCRIPT RODA EM MAC...
+# site='https://earthquake.usgs.gov/fdsnws/event/1/query?format=text' 			# Qualquer magnitude
 
-# Avisará todos os terremotos acima de magnitude 2. 
-# Caso quiera alterar a magnitude, modificar linhas 13 e 15:
-
+site='https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&minmagnitude=5'	# Avisa se Mag. é >= 5.0
 hora_anterior=''
 
 while :
 do
 
-	hora=$(curl -sX GET 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&minmagnitude=2&starttime=yesterday' | awk -F'|' '{print $2 "|" $11 "|" $9 "|" $13}' | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f2)
+	hora=$(curl -sX GET ${site} | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f3)
 
-	local=$(curl -sX GET 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&minmagnitude=2&starttime=yesterday' | awk -F'|' '{print $2 "|" $11 "|" $9 "|" $13}' | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f4)
+ 	local=$(curl -sX GET ${site} | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f1)
+
+	mag=$(curl -sX GET ${site} | sed '1d; s/T/|/' | head -n1 | cut -d"|" -f12) 
 		
-	[[ $hora == $hora_anterior ]] || { afplay beep.mp3; hora_anterior=$hora; open -a Safari https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
+	[[ $hora == $hora_anterior ]] || { omxplayer beep.mp3 &> /dev/null; hora_anterior=$hora; chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive &> /dev/null; }
+	# Outros sistemas ver no rodapé...
+
+	echo "Último sismo: ${hora} UTC - ${mag}" #>> registros.txt
+	sleep 10
+
+done
+
+	# OUTROS SISTEMAS:
+	
+	# No Mac:	
+	# [[ $hora == $hora_anterior ]] || { afplay beep.mp3; hora_anterior=$hora; open -a Safari https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
 
 	# No Raspberry Pi:
 	# [[ $hora == $hora_anterior ]] || { omxplayer beep.mp3; hora_anterior=$hora; chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
@@ -23,11 +34,6 @@ do
 	# [[ $hora == $hora_anterior ]] || { cvlc --play-and-exit beep.mp3 ; hora_anterior=$hora; chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#executive; }
 
 
-
-
-	sleep 10
-
-done
-
+# Autor: Hélio Giroto
 
 
