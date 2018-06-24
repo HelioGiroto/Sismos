b6ac23ebb981823b58ad181d024ac182392fc6d6
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-06-24 20:04:20 -0300

    Update qtde_sismos.sh

--- qtde_sismos.sh
+++ qtde_sismos.sh
@@ -12,6 +12,8 @@ dados=$(echo ${mag[*]} | sed 's/ /,/g')
 # Abaixo deveria gerar um grafico jpg ou png e depois abrir o jpg/png
 echo "png()" > graf.R
 echo "plot(c("$dados"), type='l')" >> graf.R
+# ou:
+# echo "barplot(c("$dados"))" >> graf.R
 echo "dev.off()" >> graf.R
 
 # Tente tb type= l,p,b,o,c,h,s,S
