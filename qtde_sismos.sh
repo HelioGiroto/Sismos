8b40a226252e38c389499bdcc8467fa2ed6cc25b
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-06-24 19:55:19 -0300

    Add files via upload
    Script que gera um arquivo e gráfico em R.

---
+++ qtde_sismos.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+for num in $(seq 5 9)
+do
+	mag[$num]=$(curl -s -X GET $(echo 'https://earthquake.usgs.gov/fdsnws/event/1/count?minmagnitude='$num'&starttime=2000-01-01')) 
+	echo "Magnitude" $num " - " ${mag[$num]} "vezes."
+
+done
+
+dados=$(echo ${mag[*]} | sed 's/ /,/g')
+
+# Abaixo deveria gerar um grafico jpg ou png e depois abrir o jpg/png
+echo "png()" > graf.R
+echo "plot(c("$dados"), type='l')" >> graf.R
+echo "dev.off()" >> graf.R
+
+# 	Tente tb type= l,p,b,o,c,h,s,S
+
+# Esses 2 abaixo nao funcionaram: 
+# R < graf.R --no-save
+# Rscript graf.R
+
+R -f graf.R &> /dev/null
+display *.png &
+
+# Para abrir uma imagem:
+# display graf.png
+# ou pode ser xdg-open para abrir qualquer arquivo pois o xdg-open simula os dois cliques
+
+
+
+
+# Pausa nao funcionou
+# echo "Sys.sleep(20)" >> graf.R
+
