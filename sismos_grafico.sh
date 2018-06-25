f35990563749b8d3c1405c38f082f4fa5ebdf889
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-06-24 22:17:52 -0300

    Update sismos_grafico.sh

--- sismos_grafico.sh
+++ sismos_grafico.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Este script cria arquivo e gráfico em R (requer Linguagem R instalada).
 # Criará um gráfico de barras com a quantidade de Sismos no mundo desde 2000 - dividido em magnitudes...
+# Para executá-lo digite no seu Terminal Linux:
+# curl -s https://raw.githubusercontent.com/HelioGiroto/Sismos/master/sismos_grafico.sh | bash
 
 for num in $(seq 5 9)
 do
@@ -12,13 +14,12 @@ dados=$(echo ${mag[*]} | sed 's/ /,/g')		# Cria array formatado para R
 echo;echo $dados 
 
 # Antiga versão:
-# Abaixo deveria gerar um grafico jpg ou png e depois abrir o jpg/png
 # echo "png()" > graf.R
 # echo "plot(c("$dados"), type='l')" >> graf.R
 # echo "dev.off()" >> graf.R
 # Tente tb type= l,p,b,o,c,h,s,S
 
-# Daqui em diante, cria um arquivo R
+# Daqui em diante, cria um arquivo R:
 echo "png()" > graf.R
 echo "valores <- c(${dados})" >> graf.R
 echo "X <- c('>5','>6','>7','>8','>9')" >> graf.R
