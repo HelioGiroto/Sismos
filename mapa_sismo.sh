769b36b90c1d354b721e155a7e6f4b37cef128d0
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-12-02 21:01:37 -0200

    ABRE MAPA DE ONDE OCORREU O SISMO

--- mapa_sismo.sh
+++ mapa_sismo.sh
@@ -3,8 +3,9 @@
 # 	ESTE SCRIPT ABRIRÁ O NAVEGADOR NO MAPA DO SISMO DO LUGAR QUE ESTÁ A REFERÊNCIA EM CLIPBOARD
 
 local=$(xclip -o)
+firefox https://earthquake.usgs.gov/earthquakes/eventpage/${local}#map
 
-chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#map
+# chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#map
 
 
 
