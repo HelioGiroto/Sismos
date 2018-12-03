8f8837bcf890ffd80d5b9977cdb8d0ddd119b044
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-12-02 22:49:43 -0200

    ABRE MAPA DE ONDE OCORREU O SISMO

--- mapa_sismo.sh
+++ mapa_sismo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 	ESTE SCRIPT ABRIRÁ O NAVEGADOR NO MAPA DO SISMO DO LUGAR QUE ESTÁ A REFERÊNCIA EM CLIPBOARD
-
+# É necessário instalar o xclip, digitando no Terminal: sudo apt-get install xclip
 local=$(xclip -o)
 
 # chromium-browser https://earthquake.usgs.gov/earthquakes/eventpage/${local}#map 
